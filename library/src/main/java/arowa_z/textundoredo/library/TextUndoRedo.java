c2052432acec62df0698291b8f630c0327183402
Arowa_Z <Arowa_Z@qq.com>
2016-11-16 19:17:47 +0800

    Initial commit

---
+++ library/src/main/java/arowa_z/textundoredo/library/TextUndoRedo.java
@@ -0,0 +1,140 @@
+package arowa_z.textundoredo.library;
+
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+/**
+ * 实现文本撤销重做功能
+ * Created by Arowa_Z on 2016/4/26.
+ */
+public class TextUndoRedo implements TextWatcher {
+    public interface TextChangInfo {
+        void textAction();
+    }
+
+    private class Record {
+        private int start;
+        private int end;
+        private CharSequence text;
+        private Record prior;
+        private Record next;
+
+        Record(int start, int end, CharSequence text) {
+            this.start = start;
+            this.end = end;
+            this.text = text;
+            if (offset != null) {
+                offset.next = this;
+                prior = offset;
+            }
+            offset = this;
+        }
+    }
+
+    private Record offset;
+    private Editable editable;
+    private TextChangInfo info;
+    private boolean isUndoOrRedo;
+
+    public TextUndoRedo(EditText editText, TextChangInfo info) {
+        editText.addTextChangedListener(this);
+        this.editable = editText.getEditableText();
+        this.info = info;
+        new Record(0, 0, null);
+    }
+
+    public void exeUndo() {
+        exeUndoOrRedo(true);
+    }
+
+    public void exeRedo() {
+        exeUndoOrRedo(false);
+    }
+
+    public boolean canUndo() {
+        return canUndoOrRedo(true);
+    }
+
+    public boolean canRedo() {
+        return canUndoOrRedo(false);
+    }
+
+    public void cleanRecord(){
+        cleanPrior();
+        cleanNext();
+    }
+
+    //==============================================================================================
+
+    private void noticeTextChang(){
+        if(info != null){
+            info.textAction();
+        }
+    }
+
+    private void cleanNext() {
+        while (offset.next != null) {
+            Record record = offset.next;
+            offset.next = record.next;
+            record.prior = null;
+            record.next = null;
+        }
+    }
+
+    private void cleanPrior() {
+        while (offset.prior != null) {
+            Record record = offset.prior;
+            offset.prior = record.prior;
+            record.prior = null;
+            record.next = null;
+        }
+    }
+
+    private boolean canUndoOrRedo(boolean Or) {
+        if (Or) {
+            return offset.prior != null;
+        } else {
+            return offset.next != null;
+        }
+    }
+
+    private void exeUndoOrRedo(boolean Or) {
+        if (!Or) {
+            offset = offset.next;
+        }
+        isUndoOrRedo = true;
+        CharSequence temp = editable.subSequence(offset.start, offset.end);
+        editable.replace(offset.start, offset.end, offset.text);
+        offset.end = offset.start + offset.text.length();
+        Selection.setSelection(editable, offset.end);
+        offset.text = temp;
+        isUndoOrRedo = false;
+        if (Or) {
+            offset = offset.prior;
+        }
+        noticeTextChang();
+    }
+
+    @Deprecated
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        if (isUndoOrRedo) {
+            return;
+        }
+        new Record(start, start + after, s.subSequence(start, start + count));
+        cleanNext();
+        noticeTextChang();
+    }
+
+    @Deprecated
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Deprecated
+    @Override
+    public void afterTextChanged(Editable s) {
+    }
+}
---
+++ sample/src/main/java/arowa_z/textundoredo/sample/MainActivity.java
@@ -0,0 +1,53 @@
+package arowa_z.textundoredo.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import arowa_z.textundoredo.library.TextUndoRedo;
+
+public class MainActivity extends Activity implements
+        TextUndoRedo.TextChangInfo,
+        View.OnClickListener {
+    private TextUndoRedo TUR;
+    private Button btn_undo;
+    private Button btn_redo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_undo = (Button) findViewById(R.id.btn_undo);
+        btn_undo.setOnClickListener(this);
+        btn_redo = (Button) findViewById(R.id.btn_redo);
+        btn_redo.setOnClickListener(this);
+        findViewById(R.id.btn_clean).setOnClickListener(this);
+
+        TUR = new TextUndoRedo((EditText) findViewById(R.id.et_area), this);
+        textAction();
+    }
+
+    @Override
+    public void textAction() {
+        btn_undo.setEnabled(TUR.canUndo());
+        btn_redo.setEnabled(TUR.canRedo());
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_undo:
+                TUR.exeUndo();
+                break;
+            case R.id.btn_redo:
+                TUR.exeRedo();
+                break;
+            case R.id.btn_clean:
+                TUR.cleanRecord();
+                textAction();
+                break;
+        }
+    }
+}
